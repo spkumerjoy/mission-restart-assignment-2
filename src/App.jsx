b3fae828206fa74278495ef4e87ac92d97981454
spkumerjoy <joyskydd@gmail.com>
2026-03-05 00:40:12 +0600

    Navbar Design Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCalendar } from "@fortawesome/free-regular-svg-icons";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
     return (
         <>
-            <div className="card bg-base-100 w-96 shadow-sm">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"
-                    />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Card Title <FontAwesomeIcon icon={faCalendar} />
-                    </h2>
-
-                    <p>
-                        A card component has a figure, a body part, and inside
-                        body there are title and actions parts
-                    </p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+            <Navbar></Navbar>
         </>
     );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="w-11/12 mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost lg:hidden"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                {" "}
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />{" "}
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                        >
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li>
+                                <a>FAQ</a>
+                            </li>
+                            <li>
+                                <a>Changelog</a>
+                            </li>
+                            <li>
+                                <a>Blog</a>
+                            </li>
+                            <li>
+                                <a>Download</a>
+                            </li>
+                            <li>
+                                <a>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl font-bold">
+                        CS-Ticket System
+                    </a>
+                </div>
+                <div className="navbar-end">
+                    <div className="hidden lg:flex me-5">
+                        <ul className="menu menu-horizontal px-1">
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li>
+                                <a>FAQ</a>
+                            </li>
+                            <li>
+                                <a>Changelog</a>
+                            </li>
+                            <li>
+                                <a>Blog</a>
+                            </li>
+                            <li>
+                                <a>Download</a>
+                            </li>
+                            <li>
+                                <a>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <a
+                        className="
+                    bg-gradient-to-br from-[#632EE3] to-[#9F62F2]
+                    btn text-white font-medium hover:opacity-90 transition"
+                    >
+                        <FontAwesomeIcon icon={faPlus} /> New Ticket
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
